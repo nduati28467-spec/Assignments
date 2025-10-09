c23816a72cbcf439448cdd785f8e14640148f999
nduati28467-spec <nduati.28467@students.kyu.ac.ke>
2025-10-09 16:15:30 +0300

    Add files via upload

---
+++ Password verification (1).c
@@ -0,0 +1,21 @@
+/*
+NAME- Sharon Murugi Nduati 
+REG NO- CT101/G/28467/25
+COURSE-COMPUTER SCIENCE,group A
+DISCRIPTION-Passsword verification
+ 
+*/
+#include<stdio.h>
+int main(){
+	
+	int password=0000;
+	
+	do
+	{
+		printf("enter your password:");
+		scanf("%d",& password);
+	}
+	while(password!=0000);
+	printf("access granted",password=0000);
+	return 0;
+}
