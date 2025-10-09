d0cd5e13eb18ba665dc0862a0cee379ac52128fd
nduati28467-spec <nduati.28467@students.kyu.ac.ke>
2025-10-09 11:00:29 +0300

    Add files via upload

---
+++ descending order.c
@@ -0,0 +1,19 @@
+/*
+NAME-SHARON Murugi 
+REG NO-CT101/G/28467/25
+COURSE-COMPUTER SCIENCE,group A
+DISCRIPTION-DESCENDING ORDER
+ 
+*/
+#include<stdio.h>
+//main function
+int main(){
+	
+	int numbers;
+	for(numbers=100;numbers>=50;numbers--)
+	{
+		printf("\n %d",numbers);
+		
+	}
+	return 0;
+}
